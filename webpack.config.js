b193ab7011d9e091bb671325aefec787939cc3c0
Artemyeva Anastasiya <art-nastya@inbox.ru>
2021-06-29 00:23:56 +0300

    fix paes

--- webpack.config.js
+++ webpack.config.js
@@ -33,11 +33,11 @@ module.exports = {
         errorDetails: true,
     },
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'docs'),
         filename: 'js/index.js',
     },
     devServer: {
-        contentBase: path.resolve(__dirname, 'dist/'),
+        contentBase: path.resolve(__dirname, 'docs/'),
         compress: true,
         port: 9000,
         host: '0.0.0.0',
@@ -92,11 +92,11 @@ module.exports = {
             patterns: [
                 {
                     from: path.resolve(__dirname, 'src/img'),
-                    to: path.resolve(__dirname, 'dist/img'),
+                    to: path.resolve(__dirname, 'docs/img'),
                 },
                 {
                     from: path.resolve(__dirname, 'src/lightgallery'),
-                    to: path.resolve(__dirname, 'dist/lightgallery'),
+                    to: path.resolve(__dirname, 'docs/lightgallery'),
                 }
             ],
         }),
@@ -127,7 +127,7 @@ module.exports = {
                 ],
             },
             {
-                test: /\.(ttf|eot|woff|woff2|svg|gif|otf)(\?v=[0-9]\.[0-9]\.[0-9])?$/i,
+                test: /\.(ttf|eot|woff|woff2|gif|otf)(\?v=[0-9]\.[0-9]\.[0-9])?$/i,
                 use: [{
                     loader: 'file-loader',
                 options: {
@@ -138,7 +138,7 @@ module.exports = {
                 }]
             },
             {
-                test: /\.(jpe?g|png|gif|mp3)$/i,
+                test: /\.(jpe?g|png|gif|svg|mp3)$/i,
                 use: [{
                     loader: 'file-loader',
                     options: {
@@ -156,93 +156,3 @@ module.exports = {
         ],
     },
 };
-
-// const path = require('path');
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
-// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-// const CopyPlugin = require('copy-webpack-plugin');
-//
-// module.exports = {
-//     stats: {
-//         children: true,
-//     },
-//     context: path.resolve(__dirname, 'src'),
-//     mode: "development",
-//     entry: ['@babel/polyfill', './js/index.js'],
-//     output: {
-//         path: path.resolve(__dirname, 'docs'),
-//         filename: 'js/index.js',
-//     },
-//     plugins: [
-//         new HtmlWebpackPlugin({
-//             filename: 'index.html',
-//             template: 'index.html',
-//         }),
-//         new CopyPlugin({
-//             patterns: [
-//                 {
-//                     from: path.resolve(__dirname, 'src/img'),
-//                     to: path.resolve(__dirname, 'docs/img'),
-//                 }
-//             ],
-//         }),
-//         new MiniCssExtractPlugin({
-//             filename: 'index.css',
-//         }),
-//     ],
-//     devServer: {
-//         contentBase: './docs',
-//         compress: true,
-//         port: 9000,
-//         host: '0.0.0.0',
-//         disableHostCheck: true,
-//     },
-//     resolve: {
-//         alias: {
-//             '@': path.resolve(__dirname, 'src'),
-//         },
-//     },
-//
-//     devtool: 'inline-source-map',
-//     module: {
-//         rules: [
-//             {
-//                 test: /\.css$/i,
-//                 use: [ MiniCssExtractPlugin.loader, 'css-loader'],
-//             },
-//             {
-//                 test: /\.(ttf|eot|woff|woff2|svg|gif|otf)(\?v=[0-9]\.[0-9]\.[0-9])?$/i,
-//                 use: [{
-//                     loader: 'file-loader',
-//                 options: {
-//                     name: '[name].[ext]',
-//                     outputPath: 'fonts/',
-//                     publicPath: 'fonts/',
-//                 }
-//                 }]
-//             },
-//             {
-//                 test: /\.(jpe?g|png|gif|mp3)$/i,
-//                 use: [{
-//                     loader: 'file-loader',
-//                     options: {
-//                         name: '[name].[ext]',
-//                         outputPath: 'img/',
-//                         publicPath: 'img/',
-//                     }
-//                 }]
-//             },
-//             {
-//                 test: /\.m?js$/,
-//                 exclude: /(node_modules)/,
-//                 use: {
-//                     loader: 'babel-loader',
-//                     options: {
-//                         presets: ['@babel/preset-env'],
-//                         plugins: ['@babel/plugin-proposal-object-rest-spread'],
-//                     }
-//                 }
-//             }
-//         ]
-//     }
-// };
